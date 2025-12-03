be74ad2dd9af8b979f647e69ad0f60eda6598ce9
its-me-meow <btj1011@naver.com>
2025-12-03 13:19:54 +0900

    🐛 fix: 배포 후 빈 화면 오류 해결2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/asbestos_FE/',
+  base: process.env.NODE_ENV === 'production' ? '/asbestos_FE/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
